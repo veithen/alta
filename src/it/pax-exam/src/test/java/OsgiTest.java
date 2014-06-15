de60193ed19e23045d752d787d26270eeb84bb65
Andreas Veithen <andreas.veithen@gmail.com>
2014-06-15 23:20:27 +0100

    Complete the Pax Exam integration test

--- src/it/pax-exam/src/test/java/OsgiTest.java
+++ src/it/pax-exam/src/test/java/OsgiTest.java
@@ -17,17 +17,37 @@
  * limitations under the License.
  * #L%
  */
+import static org.junit.Assert.assertNotNull;
+import static org.ops4j.pax.exam.CoreOptions.junitBundles;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.url;
+
+import javax.inject.Inject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Configuration;
+import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.osgi.service.cm.ConfigurationAdmin;
 
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
 public class OsgiTest {
+    @Configuration
+    public static Option[] configuration() {
+        return options(
+            url("link:classpath:org.apache.felix.configadmin.link"),
+            junitBundles());
+    }
+    
+    @Inject
+    private ConfigurationAdmin configAdmin;
+    
     @Test
     public void test() {
-        
+        assertNotNull(configAdmin);
     }
 }
